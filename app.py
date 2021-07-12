6f6df892c1be3ad446dddc0a73f9106b97942890
Fatema-Alam <85313035+Fatema-Alam@users.noreply.github.com>
2021-07-12 22:02:23 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+import json
+import requests
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello, Booo!"
+
+
+@app.route("/<solution>}")
+def predict(solution):
+    return "solution"
+
